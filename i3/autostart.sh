7bc19b362ff35796df1e68c9137c63e3003474bb
anshulc95 <anshulchauhan@outlook.com>
2015-02-08 20:09:24 +0530

    rotate wallpaper script

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -1,7 +1,7 @@
 xset -b &
 xrdb ~/.Xdefaults
 # feh --bg-fill /home/ans/Downloads/Wallpaper/wallhaven-16149.png &
-# nitrogen --restore
-sh /home/ans/.i3/rotate-wallpaper.sh
+nitrogen --restore
+#sh /home/ans/.i3/rotate-wallpaper.sh
 xcompmgr &
 nm-applet &
