9b6e1c4f81a18ea3d7f618f95bbbfdd1a523afc3
Anshul Chauhan <anshul@marchtee.com>
2021-06-28 05:01:45 +0530

    Remove all files
    Because I have not put these dotfiles to use in a long time.
    It's time to get rid of them and add new configs that I actually use.

--- scripts/flux.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-xflux -l 18.6 -g 73.4
--- scripts/pacg.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pacgraph -p 12 60 -n -b "#000000" -t "#553333" -d "#333355" -l "#335533" -f /home/vedexent/wallpaper/pacgraph
-inkscape --export-png=/home/vedexent/wallpaper/pacgraph.png /home/vedexent/wallaper/pacgraph.svg
-feh --bg-max /home/vedexent/wallpaper/pacgraph.png
